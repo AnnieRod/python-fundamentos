7ad52e91671a4b568a6d9ecfba8d3033c7471e8b
Ana M. Rodriguez <108434764+AnnieRod@users.noreply.github.com>
2022-08-18 19:38:16 -0500

    instancias con import de modulos

---
+++ DojoPets/main.py
@@ -0,0 +1,14 @@
+from dojopets import Ninja
+from pets import Cat, Pet
+
+#Creación de instancias
+mimo = Pet("Duck", "carrots",20,20,"quack quack")
+annie = Ninja("Annie","Rod","cookies", "grains",mimo) #se puede definir aqui dentro la mascota?
+
+annie.walk().feed().wash().nap_time().pet_data()
+
+#Instancia con mascota de class hija (ejemplo de herencia y polimorfismo) -AUN NO FUNCIONA!!!, no incluye al gato como mascota ni se apoya en el diccionario para crearlo
+enzo = Cat(type="Enzo", snacks="tuna", energy=60, health=60, sound="meeeow")
+carpe = Ninja("Carpe","Diem","peanuts", "cat-chow", enzo)  
+
+carpe.walk().feed().wash().nap_time().pet_data()
